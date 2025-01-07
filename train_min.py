d1383fe315d7c3b94ea0b2d88d654932846edcbe
Simon Bachhuber <Simon.Bachhuberr@gmail.com>
2025-01-07 15:18:08 +0100

    adds `train.py` without the plotting / gif stuff

---
+++ train_min.py
@@ -0,0 +1,15 @@
+import gymnasium as gym
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env.vec_frame_stack import VecFrameStack
+
+import rand_dyn_env  # noqa: F401
+
+env = gym.make(
+    "RandDyn-v0",
+    on_reset_draw_motion=True,
+    on_reset_draw_sys=True,
+)
+env = VecFrameStack(make_vec_env(env, 1), 6)
+model = PPO("MultiInputPolicy", env)
+model.learn(total_timesteps=1_000_000)
