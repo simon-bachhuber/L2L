7bfce535c151ba0487fb9d04dacf5b49e4e884f3
Simon Bachhuber <Simon.Bachhuberr@gmail.com>
2025-01-29 09:45:35 +0100

    make `make_vec_env` receive an `env_fn` and not `env` object directly

--- train_min.py
+++ train_min.py
@@ -5,11 +5,15 @@ from stable_baselines3.common.vec_env.vec_frame_stack import VecFrameStack
 
 import rand_dyn_env  # noqa: F401
 
-env = gym.make(
-    "RandDyn-v0",
-    on_reset_draw_motion=False,
-    on_reset_draw_sys=False,
-)
-env = VecFrameStack(make_vec_env(env, 1), 6)
+
+def env_fn():
+    return gym.make(
+        "RandDyn-v0",
+        on_reset_draw_motion=False,
+        on_reset_draw_sys=False,
+    )
+
+
+env = VecFrameStack(make_vec_env(env_fn, 1), 6)
 model = PPO("MultiInputPolicy", env)
 model.learn(total_timesteps=1_000_000)
